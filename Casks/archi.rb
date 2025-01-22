c538d4179c9dd7dbb2b3dba38ba3565f88eca294
Martin Mauch <martin.mauch@gmail.com>
2025-01-22 13:33:15 +0100

    update: Archi 5.4.3

--- Casks/archi.rb
+++ Casks/archi.rb
@@ -1,8 +1,7 @@
 cask "archi" do
-  version "5.0.2"
+  version "5.4.3"
   sha256 :no_check
-
-  url "https://www.archimatetool.com/downloads/archi.php?/#{version}/Archi-Mac-Silicon-#{version}.dmg"
+  url "https://www.archimatetool.com/downloads/archi/#{version}/Archi-Mac-Silicon-#{version}.dmg"
   name "archi"
   desc "The Open Source modelling toolkit for creating ArchiMate models and sketches. Used by Enterprise Architects everywhere."
   homepage "https://www.archimatetool.com/"
