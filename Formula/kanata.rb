63820f023479a318fbe99608f0a378350d9bf2a4
Martin Mauch <martin@mauch.dev>
2025-02-05 09:59:13 +0100

    update: Kanata 1.7.0

--- Formula/kanata.rb
+++ Formula/kanata.rb
@@ -1,9 +1,9 @@
 class Kanata < Formula
   desc "Improve keyboard comfort and usability with advanced customization"
   homepage "https://github.com/jtroo/kanata"
-  url "https://github.com/jtroo/kanata/archive/refs/tags/v1.6.1.tar.gz"
-  version "1.6.1"
-  sha256 "c0e047197af23cf434adf20e21a871b7b12c876b58ac75852d662c004bf49f2c"
+  version "1.7.0"
+  url "https://github.com/jtroo/kanata/archive/refs/tags/v#{version}.tar.gz"
+  sha256 "eb7e11511f77558d72b5b3b0c9defb04b269637e5c8a4ad9b45d21382e9247d2"
   license "LGPL-3.0-or-later"
   head "https://github.com/jtroo/kanata.git", branch: "main"
 
